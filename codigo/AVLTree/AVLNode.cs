4ca0e68e61cd214e675eabcb9c98afff40b754ee
Natália Santana Barbosa <barbosa.nataliasantana@gmail.com>
2018-11-04 20:54:22 -0200

    Estrutura AVLTree

---
+++ codigo/AVLTree/AVLNode.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AVLTree 
+{
+    public sealed class AvlNode<TKey, TValue>
+	{
+		public AvlNode<TKey, TValue> Parent;
+		public AvlNode<TKey, TValue> Left;
+		public AvlNode<TKey, TValue> Right;
+		public TKey Key;
+		public TValue Value;
+		public int Balance;
+    }
+
+}
